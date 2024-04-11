4b9adacae7efe42fcd4deba02b9ecae1d40b8a53
Stella Hwang <sjhwang@usc.edu>
2024-04-11 23:42:57 +0000

    finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,58 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts, 
+    DailySchedule sched,
+    size_t day,
+    int worker,
+    std::vector<size_t>& workerShifts
+) {
+    if (day == avail.size()) { // base case: all days scheduled
+      return true;
+    }
+    if (worker == workerShifts.size()) { // no more workers
+      return false;
+    }
+
+    // worker cannot work today
+    if (!avail[day][worker] || workerShifts[worker] >= maxShifts) {
+      return schedHelper(avail, dailyNeed, maxShifts, sched, day, worker+1, workerShifts);
+    }
+
+    // if worker is avail. and doesn't exceed max shifts
+    // there are two options here, we can schedule the worker, or we can not schedule the worker
+    // schedule the worker for the curr day
+    sched[day].push_back(worker);
+    // update worker shifts 
+    workerShifts[worker]++;
+
+    // move on to the next day if daily need is filled, otherwise move on to the next worker
+    size_t nextDay;
+    int nextWorker;
+    if (dailyNeed == sched[day].size()) {
+      nextDay = day+1;
+      nextWorker = 0;
+    } else {
+      nextDay = day;
+      nextWorker = worker+1;
+    }
+
+    // next day 
+    if (schedHelper(avail, dailyNeed, maxShifts, sched, nextDay, nextWorker, workerShifts)) {
+        return true; // valid sched found 
+    }
+
+    // backtrack-- remove worker from curr day's sched
+    sched[day].pop_back();
+    // restore worker shifts
+    workerShifts[worker]--;
+    
+    // don't schedule this worker for today
+    return schedHelper(avail, dailyNeed, maxShifts, sched, day, worker+1, workerShifts);
+}
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -35,11 +87,18 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
+
+    // if availbility matrix is empty
+    if(avail.empty()) {
+        return false;
+    }
+    
     sched.clear();
     // Add your code below
 
-
-
+    sched.resize(avail.size());
+    std::vector<size_t> workerShifts(avail[0].size(), 0);
+    return schedHelper(avail, dailyNeed, maxShifts, sched, 0, 0, workerShifts);
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,39 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void generateWords(std::string currWord, std::string floating, const std::set<std::string>& dict, int pos, std::set<std::string>& result) {
+    if (pos == currWord.length()) {
+        if (dict.find(currWord) != dict.end() && floating.length() == 0) {
+            result.insert(currWord);
+        }
+        return;
+    }
+
+    if (currWord[pos] != '-') {
+        // if not a blank, move on to the next position
+        generateWords(currWord, floating, dict, pos+1, result);
+        return;
+    }
+
+    // fill in with any character between a,z inclusive
+    int numBlanks = 0;
+    for (int k = pos; k < currWord.length(); k++) {
+        numBlanks += (int) (currWord[k] == '-');
+    }
+    // if floating is shorter than the number of blanks
+    if (floating.length() < numBlanks) {
+        for (char c = 'a'; c <= 'z'; ++c) {
+            currWord[pos] = c;
+            generateWords(currWord, floating, dict, pos+1, result);
+        }
+    }
+    // fill in with a floating character
+    for (int k = 0; k < floating.length(); k++) { // floating letters -- try all combinations 
+        std::string newFloating = floating.substr(0,k) + floating.substr(k+1);
+        currWord[pos] = floating[k];
+        generateWords(currWord, newFloating, dict, pos+1, result);
+    }
+}
 
 
 // Definition of primary wordle function
@@ -22,7 +55,12 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> result;
+    std::string currWord = in;
+    std::string currFloating = floating;
+    generateWords(currWord, currFloating, dict, 0, result);
 
+    return result;
 }
 
 // Define any helper functions here
